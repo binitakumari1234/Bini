f39ab41a2c2038999ef66d1ffd8d8a59bcf5a73c
amar12072003 <amarjitpradhan4142@gmail.com>
2025-03-03 21:55:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 from os import environ 
 
@@ -9,13 +6,10 @@ class Config:
     API_HASH = environ.get("API_HASH", "d858470de0403d3479bd1a0c1fbf2625")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7900068504:AAGXEA1jFXhoJOIdEVN-VzB55bqNFlWWqOw") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://amarjitpradhan1000:<ZdHyuTVez6rYU78V>@cluster0.qmkwu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://amarjitpradhan1000:ZdHyuTVez6rYU78V@cluster0.qmkwu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "7167357921"))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 class temp(object): 
     lock = {}
@@ -23,7 +17,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
